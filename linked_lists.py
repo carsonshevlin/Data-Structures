9421197a81119cd37d76ddd231085494b6864315
Carson Shevlin <carsonshevlin@outlook.com>
2023-06-26 14:03:04 -0500

    Completing the append and print method

--- linked_lists.py
+++ linked_lists.py
@@ -6,5 +6,24 @@ class Node:
 class LinkedList:
     def __init__(self, head=None):
         self.head = head
-    def append(sefl, new_node):
-        pass
+    def append(self, new_node):
+        current = self.head
+        if current:
+            while current.next:
+                current = current.next
+            current.next = new_node
+        else:
+            self.head = new_node
+
+    def print(self):
+        current = self.head
+        while current:
+            print(current.value)
+            current = current.next
+
+e1 = Node(1)
+e2 = Node(2)
+
+ll = LinkedList(e1)
+ll.append(e2)
+ll.print()
